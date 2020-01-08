dd89fa16b44e59725e03d34330535f8c440c3c64
Alejandro Garrido <alejandro.garrido@boream.com>
2020-01-08 06:06:07 +0100

    WIP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Injector, DoBootstrap } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
 
 import { AppComponent } from './app.component';
+import { ButtonComponent } from './button/button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  entryComponents: [ButtonComponent],
+  providers: []
 })
-export class AppModule { }
+export class AppModule implements DoBootstrap {
+  constructor(private injector: Injector) { }
+
+  ngDoBootstrap() {
+    const customElement = createCustomElement(ButtonComponent, { injector: this.injector });
+    customElements.define('app-button', customElement);
+  }
+}
